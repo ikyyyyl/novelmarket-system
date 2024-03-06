3c176739396b5f096e1245d3b660cd9067b36f96
mikay <fabromikylla@gmail.com>
2024-03-06 09:13:40 +0800

    MASTER

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Create a Product</h1>
+    <div>
+        @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+        @endif
+    </div>
+    <form method = "post" action = "{{route('product.store')}}">
+        @csrf
+        @method('post')
+        <div>
+            <label>Title: </label>
+            <input type = "text" name = "title" placeholder = "title" />
+        </div>
+        <div>
+            <label>Author: </label>
+            <input type = "text" name = "author" placeholder = "author" />
+        </div>
+        <div>
+            <label>Genre: </label>
+            <input type = "text" name = "genre" placeholder = "genre" />
+        </div>
+        <div>
+            <label>Description: </label>
+            <input type = "text" name = "desc" placeholder = "description" />
+        </div>
+        <div>
+            <label>Price: </label>
+            <input type = "text" name = "price" placeholder = "price" />
+        </div>
+        <div>
+            <label>Quantity: </label>
+            <input type = "text" name = "quantity" placeholder = "quantity" />
+        </div>
+        <div>
+            <input type = "submit" value = "Save a New Product" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Product</h1>
+    <div>
+        @if(session()->has('success'))
+        <div>
+            {{session('success')}}
+        </div> 
+        @endif
+    </div>
+    <div>
+        <div>
+            <a href = "{{route('product.create')}}">Create a Product</a>
+        </div>
+        <table border = "2">
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Genre</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            @foreach($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->title}}</td>
+                    <td>{{$product->author}}</td>
+                    <td>{{$product->genre}}</td>
+                    <td>{{$product->desc}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->quantity}}</td>
+                    <td>
+                        <a href = "{{route('product.edit', ['product' => $product])}}">Edit</a>
+                    </td>
+                    <td>
+                        <form method="post" action="{{route('product.destroy', ['product' => $product])}}" >
+                            @csrf
+                            @method('delete')
+                            <input type="submit" value = "Delete" />
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+    
+</body>
+</html>
